6e79c19d3e98896df5a7c1696ed5aa617f464799
ehddnr8813 <ehddnr8813@daum.net>
2022-05-08 22:40:40 +0900

    fix acmp2632

--- acmp/acmp2632/Main.java
+++ acmp/acmp2632/Main.java
@@ -2,20 +2,15 @@ package algoStudy.acmp.acmp2632;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
 
     static int arr[], arr2[];
     static int target, M, N;
-    static int aCount[], bCount[];
-    static Map<Integer, Integer> bPizza = new HashMap<>();
-    static ArrayList<Integer> aKeys = new ArrayList<>();
-    static ArrayList<Integer> bKeys = new ArrayList<>();
+    static TreeMap<Integer, Integer> aPizza = new TreeMap<>();
+    static TreeMap<Integer, Integer> bPizza = new TreeMap<>();
 
     public static void main(String[] args) throws Exception {
 
@@ -36,43 +31,43 @@ public class Main {
             arr2[i] = Integer.parseInt(br.readLine());
         }
 
-        int aTotal = 0;
-        for (int i = 0; i < arr.length; i++) {
-            aTotal += arr[i];
-        }
-
-        int bTotal = 0;
-        for (int i = 0; i < arr2.length; i++) {
-            bTotal += arr2[i];
-        }
-
-        aCount = new int[Math.max(aTotal, target) + 1];
-        bCount = new int[Math.max(bTotal, target) + 1];
-
         for (int i = 0; i < arr.length; i++) {
             int temp = 0;
             for (int j = i; j < i + arr.length - 1; j++) {
                 temp += arr[j % arr.length];
-                aCount[temp]++;
+                aPizza.put(temp, aPizza.getOrDefault(temp, 0) + 1);
             }
         }
+
+        int aTtotal = 0;
+        for (int i = 0; i < arr.length; i++) {
+            aTtotal += arr[i];
+        }
+        aPizza.put(aTtotal, 1);
 
         for (int i = 0; i < arr2.length; i++) {
             int temp = 0;
             for (int j = i; j < i + arr2.length - 1; j++) {
                 temp += arr2[j % arr2.length];
-                bCount[temp]++;
+                bPizza.put(temp, bPizza.getOrDefault(temp, 0) + 1);
             }
         }
-        aCount[aTotal] = 1;
-        bCount[bTotal] = 1;
-        aCount[0] = 1;
-        bCount[0] = 1;
 
-        int answer = 0;
-        for (int i = 0; i <= target; i++) {
-            answer += aCount[i] * bCount[target - i];
+        int bTtotal = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            bTtotal += arr2[i];
         }
+        bPizza.put(bTtotal, 1);
+        aPizza.put(0, 1);
+        bPizza.put(0, 1);
+        int answer = 0;
+
+        for (Integer key : aPizza.keySet()) {
+            if (bPizza.containsKey(target - key)) {
+                answer += aPizza.get(key) * bPizza.get(target - key);
+            }
+        }
+
         System.out.println(answer);
     }
 }
